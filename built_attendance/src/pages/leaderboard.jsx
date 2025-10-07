295c7a7e6957c732c420ddb4e0303547ccaf23b6
evelasco25 <evela25@illinois.edu>
2025-10-06 19:01:33 -0500

    Worked on welcome card

--- built_attendance/src/pages/leaderboard.jsx
+++ built_attendance/src/pages/leaderboard.jsx
@@ -3,12 +3,31 @@ import LeaderboardPlayer from "../components/LeaderboardPlayer";
 
 function Leaderboard() {
     return (
-    <div>
-        <Header></Header>
+        <div>
+        <Header />
         Leaderboard Page
-        <LeaderboardPlayer/>
+        <LeaderboardPlayer />
+
+        <div className="card">
+            <img src="img_avatar.png" alt="Avatar" style={{ width: "100%" }} />
+            <div className="container">
+                <h4><b>John Doe</b></h4>
+                <p>Architect & Engineer</p>
+            </div>
+        </div>
+        <div className="mini_card_1">
+            <div className="container">
+                <h4><b>Ranking</b></h4>
+            </div>
+        </div>
+        <div className="mini_card_2">
+            <div className="container">
+                <h4><b>Points</b></h4>
+            </div>
+        </div>
     </div>
-    );
+);
+    
 }
 
 export default Leaderboard;
