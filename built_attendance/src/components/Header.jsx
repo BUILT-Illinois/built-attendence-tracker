3edc00098f64c0cc4861a8e965dbf9b68cdf7aac
samev <samevangelista07@icloud.com>
2025-10-06 19:00:59 -0500

    Made Header

--- built_attendance/src/components/Header.jsx
+++ built_attendance/src/components/Header.jsx
@@ -1,9 +1,21 @@
 import '../assets/Header.css';
+import { Link } from 'react-router';
 
 function Header () {
     return(
-        <div>
-            Header
+        <div className='header'>
+            <div className='container'>
+                <Link className='' to="/">
+                    <img className='built-img' src='https://built-illinois.org/built-logo.png'/>
+                </Link>
+                <Link to="/leaderboard">
+                    <h2>Leaderboard</h2>
+                </Link>
+                <h2>Events</h2>
+                <h2>Check-In</h2>
+
+                <h2>Logout</h2>
+            </div>
         </div>
     );
 }
