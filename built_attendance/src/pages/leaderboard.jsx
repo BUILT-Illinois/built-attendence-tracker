50264507628ccb28897013041d9a93614d4faf77
Xavier Contreras <xavierc2@illinois.edu>
2025-10-06 19:01:03 -0500

    LeaderBoard Box

--- built_attendance/src/pages/leaderboard.jsx
+++ built_attendance/src/pages/leaderboard.jsx
@@ -1,9 +1,12 @@
 import Header from "../components/Header";
 import LeaderboardPlayer from "../components/LeaderboardPlayer";
-
+import "../assets/LeaderboardPlayer.css"
 function Leaderboard() {
     return (
-    <div>
+    <div class= "card">
+        <div class ="container">
+            <h4 style ={{color: "#f78b07c7"}}><b>Leaderboard</b></h4>
+        </div>
         <Header></Header>
         Leaderboard Page
         <LeaderboardPlayer/>
