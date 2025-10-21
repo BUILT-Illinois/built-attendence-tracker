7b6a8ef87f40061ee5111c0c071ddf2da96a81b9
evelasco25 <evela25@illinois.edu>
2025-10-20 19:00:29 -0500

    Updated Welcome Card

--- built_attendance/src/App.jsx
+++ built_attendance/src/App.jsx
@@ -3,9 +3,22 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className=''>
-      <Header/>
-      Hello World!
+    <div>
+      <div className="welcome">
+        <h1>Welcome Name</h1>
+      </div>
+      <div className="card">
+        <img src='https://built-illinois.org/built-logo.png'/>
+      </div>
+      <div className="mini_card_1">
+        <h3>Ranking</h3>
+        <p1>#2</p1>
+      </div>
+      <div className="mini_card_2">
+        <h3>Points</h3>
+        <p1>30</p1>
+      </div>
+      {/* <img src=''></> */}
     </div>
   );
 }
