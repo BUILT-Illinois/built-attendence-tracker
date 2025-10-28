57405dba5fb1f9199b104b923d06d6b9f03b05b1
samev <samevangelista07@icloud.com>
2025-10-28 01:12:55 -0500

    Basic Combining of Things

--- built_attendance/src/App.jsx
+++ built_attendance/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
+import UpcomingEvents from './components/UpcomingEvents';
 
 function App() {
   return (
     <div>
-      <Welcome/>
-      {/* <img src=''></> */}
+      <Header/>
+      <div>
+        <UpcomingEvents/>
+        <Welcome/>
+      </div>
     </div>
   );
 }
