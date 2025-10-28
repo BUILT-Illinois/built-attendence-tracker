c8d04e24b9ca8afedb8d2ddec9eabb120898894e
samev <samevangelista07@icloud.com>
2025-10-28 01:02:16 -0500

    Converted Welcome to a Component

--- built_attendance/src/App.jsx
+++ built_attendance/src/App.jsx
@@ -1,23 +1,11 @@
 import './App.css';
 import Header from './components/Header';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div>
-      <div className="welcome">
-        <h1>Welcome Name</h1>
-      </div>
-      <div className="card">
-        <img src='https://built-illinois.org/built-logo.png'/>
-      </div>
-      <div className="mini_card_1">
-        <h3>Ranking</h3>
-        <p1>#2</p1>
-      </div>
-      <div className="mini_card_2">
-        <h3>Points</h3>
-        <p1>30</p1>
-      </div>
+      <Welcome/>
       {/* <img src=''></> */}
     </div>
   );
